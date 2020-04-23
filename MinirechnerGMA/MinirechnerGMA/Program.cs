493f675b59d48be18419bd2ad789a602493fc7da
01maigab <01maigab@rgtfo-me.it>
2020-04-23 14:41:45 +0200

    Addition funktioniert

---
+++ MinirechnerGMA/MinirechnerGMA/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+             int Zahl1 = 0;
+            int Zahl2 = 0;
+            int Aktion = 0;
+            int Ergebnis = 0;
+
+            Console.WriteLine("Erste Zahl eingeben:");
+            Zahl1 = Convert.ToInt16(Console.ReadLine());
+
+            Console.WriteLine("Zweite Zahl eingeben");
+            Zahl2 = Convert.ToInt16(Console.ReadLine());
+
+            Console.WriteLine("Wähle die Aktion: Addition =1");
+            Aktion = Convert.ToInt16(Console.ReadLine());
+
+            if(Aktion == 1)
+                Ergebnis = Addieren(Zahl1, Zahl2);
+
+            
+
+            Console.WriteLine("Ergebnis = " + Ergebnis);
+            Console.ReadKey();
+        }
+
+        static int Addieren(int Zahl1, int Zahl2)
+        {
+            int Erg = Zahl1 + Zahl2;
+            return Erg;
+        }
+       
+        }
+    }
+
